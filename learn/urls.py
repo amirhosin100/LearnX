8976ed7c538857053c5293c8f786b78dd9d46f0e
amirhosin <computer.super111@gmail.com>
2025-10-08 21:40:14 +0330

    create ajax for score films

--- learn/urls.py
+++ learn/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('list/', views.learn_list, name='index'),
     path("detail/<slug:slug_learn>", views.learn_detail, name='learn_detail'),
     path("detail/<slug:slug_learn>/<int:id>",views.film_detail, name='film_detail'),
+
+    path("send_score/",views.send_score,name='send_score'),
 ]
--- learn/views.py
+++ learn/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.db.models import Prefetch
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
 
 from .models import *
 # Create your views here.
@@ -24,10 +26,39 @@ def learn_detail(request,slug_learn):
     return render(request,"learn/learn_detail.html",context)
 
 def film_detail(request,slug_learn,id):
-    film = get_object_or_404(LearnFilms,id=id)
+    film = get_object_or_404(LearnFilms,id=id,headline__learn__slug=slug_learn)
     learn = get_object_or_404(Learn,slug=slug_learn)
+    number_score = 0
+    if film in request.user.gived_score_to_films.all() :
+        number_score =FilmScores.objects.get(film_to=film,user_from=request.user).score
     context = {
         "film": film,
         "learn": learn,
+        "number_score": number_score,
     }
     return render(request,"learn/film_detail.html",context)
+
+@require_POST
+@login_required
+def send_score(request):
+    id_film = request.POST.get("film_id")
+    user = request.user
+    score = int(request.POST.get("score"))
+    film = get_object_or_404(LearnFilms,id=id_film)
+    try :
+        if user not in film.scores.all() :
+            print(True)
+            FilmScores(user_from=user,film_to=film,score=score).save()
+
+        else:
+            item = FilmScores.objects.get(film_to=film,user_from=user)
+            item.score = score
+            item.save()
+        film = LearnFilms.objects.get(id=id_film)
+        return JsonResponse({"score": film.number_score})
+
+    except :
+        return JsonResponse({"error": "Error"})
+
+
+
