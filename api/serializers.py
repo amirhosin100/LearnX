1a2a34b9884290d4faf739f498a0c7412601d1c6
amirhosin <computer.super111@gmail.com>
2025-10-17 17:44:49 +0330

    simple api

--- api/serializers.py
+++ api/serializers.py
@@ -1,21 +1,47 @@
 from rest_framework import serializers
-from learn.models import Learn
-from user.models import Teacher,User
+from blog.models import Post
+from user.models import *
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta :
+    class Meta:
         model = User
-        fields = ["first_name","last_name","username","id"]
+        fields = ["username","first_name","last_name","url","password"]
+        extra_kwargs = {
+            "password" :{"write_only":True},
+        }
 
+
+    def create(self, validated_data):
+        v = validated_data
+        user = User(username=v["username"],first_name=v["first_name"],last_name=v["last_name"])
+        user.set_password(v["password"])
+        user.save()
+        return user
 
 class TeacherSerializer(serializers.HyperlinkedModelSerializer):
-    user = serializers.HyperlinkedIdentityField(
-        view_name="api:user-detail"
-    )
     class Meta:
         model = Teacher
-        fields = ["user","address","city"]
-        # extra_kwargs = {
-        #     'url': {'view_name': 'user', 'lookup_field': 'user'},
-        #     'user': {'lookup_field': 'username'}
-        # }
+        fields = "__all__"
+
+class TicketSerializer(serializers.Serializer):
+    first_name = serializers.CharField(max_length=200)
+    last_name = serializers.CharField(max_length=200)
+    email = serializers.EmailField()
+    content = serializers.CharField(max_length=1000)
+
+
+    def save(self, **kwargs):
+        print(self.validated_data)
+
+
+    def validate_first_name(self,value):
+        if value == "amir":
+            raise serializers.ValidationError("خطا داریم")
+
+        return value
+
+class PostSerializer(serializers.ModelSerializer):
+    likes = UserSerializer(many=True)
+    class Meta:
+        model = Post
+        fields = "__all__"
--- api/views.py
+++ api/views.py
@@ -1,12 +1,49 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet,ViewSet
 from rest_framework.views import APIView
-from user.models import User
 from .serializers import *
+from rest_framework import permissions
+from rest_framework.response import Response
+from user.models import User
+from rest_framework.decorators import action
+from rest_framework import status
 
 class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+
+    @action(detail=False,methods=["POST"])
+    def get_is_staff(self,request):
+        users = self.queryset.filter(is_staff=True)
+        serializer = self.get_serializer(users,many=True,context={"request":request})
+        return Response(serializer.data)
+
 
 class TeacherViewSet(ModelViewSet):
-    queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
+    queryset = Teacher.objects.all()
+
+class PostViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    @action(detail=False,methods=["GET"],url_path="status")
+    def show_true_status(self,request):
+        status = bool(int(request.GET.get("status",1)))
+
+        if status == True or status == False :
+            posts = self.queryset.filter(status=status)
+            serializer = self.get_serializer(posts,many=True,context={"request":request})
+            return Response(serializer.data)
+        else:
+            return Response({"error":"Error"})
+
+class TicketView(APIView):
+    permission_classes = []
+    def post(self,request):
+        serializer = TicketSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "تیکت با موفقیت ثبت شد!"}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
